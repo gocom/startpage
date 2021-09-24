e2dcfd0de9efc38acdcd7e233d7b363a59137df4
Jukka Svahn <void@rah.pw>
2021-09-25 01:04:50 +0300

    Update regex

--- tests/unit/components/Clock.spec.js
+++ tests/unit/components/Clock.spec.js
@@ -5,7 +5,7 @@ test('displays time', () => {
   const wrapper = mount(Component);
   const { vm } = wrapper;
 
-  expect(String(vm.seconds)).toMatch(/^[0-6][0-9]$/);
+  expect(String(vm.seconds)).toMatch(/^[0-6]\d$/);
   expect(vm.amPm).toMatch(/(am|pm)/i);
 
   wrapper.destroy();
