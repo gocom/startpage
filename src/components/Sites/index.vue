9c2d14037376716f78bf20c2c7090f8238ff97c0
Jukka Svahn <void@rah.pw>
2021-09-28 19:33:30 +0300

    Mobile optimizations

--- src/components/Sites/index.vue
+++ src/components/Sites/index.vue
@@ -33,6 +33,8 @@
         class="site-grid"
         v-model="items"
         group="sites"
+        v-bind:delay="250"
+        v-bind:delayOnTouchOnly="true"
         v-bind:data-columns="columns"
         v-bind:data-rows="rows"
         v-bind:move="isDraggingAllowed">
